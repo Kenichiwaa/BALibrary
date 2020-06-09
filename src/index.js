349a34bbca8bc031628e9f0cb0afe2dd25d57c89
Ken Sugai <kenichisugai@gmail.com>
2020-06-09 15:59:08 -0700

    grab data from top-albums and set up framework

--- src/index.js
+++ src/index.js
@@ -1,17 +1,44 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import fetchTopAlbums from "./data/top-albums";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const card = (album) => {
+    return(
+       <div className="ui card">
+           {album['im:name'].label}
+       </div> 
+    )
+}
+
+class App extends React.Component {
+    state = {
+        albums: []
+    }
+
+    componentDidMount() {
+        fetchTopAlbums().then(res => {
+            console.log('outside', res.feed.entry)
+            this.setState({albums: res.feed.entry})
+        })
+    }
+
+  render() {
+      console.log(this.state.albums.map(v => v['im:name'].label))
+    return (
+      <div className="ui container">
+        <div className="ui secondary pointing menu">
+          <div className="right menu">
+            <button className="ui item active">Top Albums</button>
+            <button className="ui item">Favorite Albums</button>
+          </div>
+        </div>
+        <div className="ui content">
+          <div>{this.state.albums.map(v => card(v))}</div>
+        </div>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.querySelector("#root"));
